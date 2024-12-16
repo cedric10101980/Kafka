27bf44da8bc9b18be597c7ae7aca911f5ddaa8b4
D`Souza, Cedric (Cedric) <dsouzac@avaya.com>
2024-12-16 12:59:10 +0530

    Updated docs

---
+++ src/main/java/com/learning/kafka/kafkaops/KafkaClientApIsApplication.java
@@ -0,0 +1,13 @@
+package com.learning.kafka.kafkaops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaClientApIsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaClientApIsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/learning/kafka/kafkaops/KafkaClientApIsApplicationTests.java
@@ -0,0 +1,15 @@
+package com.learning.kafka.kafkaops;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("test")
+class KafkaClientApIsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
